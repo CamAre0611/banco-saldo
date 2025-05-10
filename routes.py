6365c563730bd924630a728c043df882ce3e7e3b
CamAre0611 <camilita0611@gmail.com>
2025-05-09 22:31:05 -0500

    Fix rutas y formato saldos.txt

--- routes.py
+++ routes.py
@@ -6,8 +6,10 @@ from flask import Blueprint
 from controller.saldo_controller import SaldoController
 from service.saldo_service import SaldoService
 
-RUTA_SALDOS = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../database/saldos.txt'))
-RUTA_LOG = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../database/log.txt'))
+
+RUTA_SALDOS = os.path.join(os.path.dirname(__file__), 'database/saldos.txt')
+RUTA_LOG = os.path.join(os.path.dirname(__file__), 'database/log.txt')
+
 
 saldo_bp = Blueprint('saldo', __name__)
 saldo_service = SaldoService(RUTA_SALDOS, RUTA_LOG)
