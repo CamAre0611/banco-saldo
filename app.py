d5ecc60371d110d1db159b31f0aaed766fd8b808
CamAre0611 <camilita0611@gmail.com>
2025-05-09 19:39:20 -0500

    Render: usa puerto de entorno

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import os
 from flask import Flask
-from routes import saldo_bp
+from flask_cors import CORS
+from routes import usuarios_bp  # o saldo_bp según el microservicio
 
 app = Flask(__name__)
-app.register_blueprint(saldo_bp)
+CORS(app)
+
+# Cambiar esto según el microservicio
+app.register_blueprint(usuarios_bp)  # o saldo_bp, login_blueprint, etc.
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5003)
+    port = int(os.environ.get('PORT', 5000))  # Render usa PORT, local usa 5000 o el que elijas
+    print("=== RUTAS ACTIVAS ===")
+    for rule in app.url_map.iter_rules():
+        print(rule)
+    app.run(host='0.0.0.0', port=port)
